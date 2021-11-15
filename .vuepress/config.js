5279eda5a4fec71da54c7332d74f2be49d318b74
foxdie987 <kilnnumber1+git@gmail.com>
2021-11-15 15:26:41 +0700

    edit

--- .vuepress/config.js
+++
@@ -1,9 +0,0 @@
-module.exports  =  {
-  title: 'A Notebook',
-  description: 'thử dùng VuePress',
-  theme: 'vdoing',
-  // theme: require.resolve('../../theme-vdoing'), // Use local theme package
-  themeConfig: {
-     sidebar: 'structuring' //  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义
-  }
-}
